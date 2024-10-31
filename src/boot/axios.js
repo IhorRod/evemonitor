0687b61173dd75065639e1aab5ad3e1ca1899aed
IhorRodionov <73175941+IhorRodionov@users.noreply.github.com>
2024-10-31 15:36:28 +0200

    New token structure

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,11 @@
 import axios from 'axios'
 
 const apiUrl = import.meta.env.VITE_PROXY_URL
+const token = import.meta.env.VITE_TOKEN
 
 export const api = axios.create({
-  baseURL: apiUrl
+  baseURL: apiUrl,
+  headers: {
+    'X-Token': token
+  }
 })
